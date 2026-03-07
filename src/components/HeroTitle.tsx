aad60c1b25928a1f4094e32be8d77f48465a898a
caleb <calebwieman@gmail.com>
2026-03-07 11:32:48 -0600

    Ares: Remove gear bounce, shift dust down 10px

--- src/components/HeroTitle.tsx
+++ src/components/HeroTitle.tsx
@@ -1,36 +1,13 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { useState, useEffect } from "react";
 
 interface GearWithDustProps {
   delay?: number;
 }
 
 function GearWithDust({ delay = 0 }: GearWithDustProps) {
-  const [bounceOffset, setBounceOffset] = useState(0);
-
-  // Random bounce effect
-  useEffect(() => {
-    const triggerBounce = () => {
-      setBounceOffset(-3);
-      setTimeout(() => setBounceOffset(3), 150);
-      setTimeout(() => setBounceOffset(0), 300);
-    };
-
-    const scheduleNextBounce = () => {
-      const randomDelay = 2500 + Math.random() * 1000;
-      setTimeout(() => {
-        triggerBounce();
-        scheduleNextBounce();
-      }, randomDelay);
-    };
-
-    const startTimeout = setTimeout(scheduleNextBounce, delay * 1000);
-    return () => clearTimeout(startTimeout);
-  }, [delay]);
-
-  // Dust particles - completely independent, always floating
+  // Dust particles - completely independent
   const dustParticles = Array.from({ length: 15 }, (_, i) => ({
     id: i,
     x: Math.random() * 20 - 10,
@@ -39,13 +16,34 @@ function GearWithDust({ delay = 0 }: GearWithDustProps) {
     duration: 2 + Math.random() * 2,
   }));
 
-  // Gear component - bounces
-  const BouncingGear = () => (
-    <motion.span
-      animate={{ y: bounceOffset }}
-      transition={{ type: "spring", stiffness: 400, damping: 20 }}
-      className="inline-block"
-    >
+  return (
+    <span className="inline-block relative">
+      {/* Dust - completely independent, floating above gear */}
+      {dustParticles.map((particle) => (
+        <motion.span
+          key={particle.id}
+          className="absolute rounded-full bg-white pointer-events-none"
+          style={{
+            width: particle.size,
+            height: particle.size,
+            left: `calc(50% + ${particle.x}px)`,
+            top: 8,
+            zIndex: 10,
+          }}
+          animate={{
+            y: [0, -12 - Math.random() * 8],
+            opacity: [0, 0.5, 0],
+          }}
+          transition={{
+            duration: particle.duration,
+            repeat: Infinity,
+            delay: particle.delay,
+            ease: "easeOut",
+          }}
+        />
+      ))}
+      
+      {/* Gear - no bounce */}
       <motion.svg
         viewBox="0 0 24 24"
         className="w-[0.7em] h-[0.7em] inline-block"
@@ -66,38 +64,6 @@ function GearWithDust({ delay = 0 }: GearWithDustProps) {
           d="M12 15.5A3.5 3.5 0 0 1 8.5 12 3.5 3.5 0 0 1 12 8.5a3.5 3.5 0 0 1 3.5 3.5 3.5 3.5 0 0 1-3.5 3.5m7.43-2.53c.04-.32.07-.64.07-.97 0-.33-.03-.66-.07-1l2.11-1.63c.19-.15.24-.42.12-.64l-2-3.46c-.12-.22-.39-.31-.61-.22l-2.49 1c-.52-.39-1.06-.73-1.69-.98l-.37-2.65A.506.506 0 0 0 14 2h-4c-.25 0-.46.18-.5.42l-.37 2.65c-.63.25-1.17.59-1.69.98l-2.49-1c-.22-.09-.49 0-.61.22l-2 3.46c-.13.22-.07.49.12.64L4.57 11c-.04.34-.07.67-.07 1 0 .33.03.65.07.97l-2.11 1.66c-.19.15-.25.42-.12.64l2 3.46c.12.22.39.3.61.22l2.49-1.01c.52.4 1.06.74 1.69.99l.37 2.65c.04.24.25.42.5.42h4c.25 0 .46-.18.5-.42l.37-2.65c.63-.26 1.17-.59 1.69-.99l2.49 1.01c.22.08.49 0 .61-.22l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.66z"
         />
       </motion.svg>
-    </motion.span>
-  );
-
-  return (
-    <span className="inline-block relative">
-      {/* Dust - completely independent, always floating above gear */}
-      {dustParticles.map((particle) => (
-        <motion.span
-          key={particle.id}
-          className="absolute rounded-full bg-white pointer-events-none"
-          style={{
-            width: particle.size,
-            height: particle.size,
-            left: `calc(50% + ${particle.x}px)`,
-            top: -2,
-            zIndex: 10,
-          }}
-          animate={{
-            y: [0, -12 - Math.random() * 8],
-            opacity: [0, 0.5, 0],
-          }}
-          transition={{
-            duration: particle.duration,
-            repeat: Infinity,
-            delay: particle.delay,
-            ease: "easeOut",
-          }}
-        />
-      ))}
-      
-      {/* Gear - bounces below the dust */}
-      <BouncingGear />
     </span>
   );
 }
